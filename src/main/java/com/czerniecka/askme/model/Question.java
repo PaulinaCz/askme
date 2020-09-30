78153869c1cfd510f0525912840c9710dc24e20f
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-09-30 10:45:26 +0100

    created question basic entity

---
+++ src/main/java/com/czerniecka/askme/model/Question.java
@@ -0,0 +1,27 @@
+package com.czerniecka.askme.model;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+public class Question {
+
+    @Id
+    private UUID questionId;
+    private UUID fromUser;
+    private String body;
+    private LocalDateTime timeQuestionAsked;
+
+    public Question(UUID fromUser, String body){
+        questionId = UUID.randomUUID();
+        this.fromUser = fromUser;
+        this.body = body;
+        this.timeQuestionAsked = LocalDateTime.now();
+    }
+
+}
