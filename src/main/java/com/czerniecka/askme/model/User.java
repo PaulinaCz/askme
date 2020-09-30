6419a164cc07200dc469645b86cd9c3b5dff3fe1
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-09-30 10:40:47 +0100

    created user basic entity

---
+++ src/main/java/com/czerniecka/askme/model/User.java
@@ -0,0 +1,32 @@
+package com.czerniecka.askme.model;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private UUID userId;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String username;
+    private LocalDateTime dateCreated;
+
+    public User(String name, String surname, String email, String password, String username) {
+        userId = UUID.randomUUID();
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.dateCreated = LocalDateTime.now();
+    }
+}
