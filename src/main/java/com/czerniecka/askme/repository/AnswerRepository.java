1f5b8bd0760c2f2c1b11d961a96f9f580a0dbde1
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-10-31 15:22:10 +0000

    reformat all UUID to Long

--- src/main/java/com/czerniecka/askme/repository/AnswerRepository.java
+++ src/main/java/com/czerniecka/askme/repository/AnswerRepository.java
@@ -4,8 +4,6 @@ import com.czerniecka.askme.model.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 }
--- src/main/java/com/czerniecka/askme/repository/CommentRepository.java
+++ src/main/java/com/czerniecka/askme/repository/CommentRepository.java
@@ -4,7 +4,6 @@ import com.czerniecka.askme.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
