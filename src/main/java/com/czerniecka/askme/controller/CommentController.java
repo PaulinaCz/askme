27bcf15d1902cf5db97db0c5f26be6b9f18ba91d
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-11-11 12:04:54 +0000

    fixed endpoint naming

--- src/main/java/com/czerniecka/askme/controller/CommentController.java
+++ src/main/java/com/czerniecka/askme/controller/CommentController.java
@@ -8,11 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/questions")
+@RequestMapping("/comments")
 public class CommentController {
 
     public CommentService commentService;
--- src/main/java/com/czerniecka/askme/dto/AskQuestionDTO.java
+++ src/main/java/com/czerniecka/askme/dto/AskQuestionDTO.java
@@ -1,10 +1,8 @@
 package com.czerniecka.askme.dto;
 
-import java.time.LocalDateTime;
 
 public class AskQuestionDTO {
 
     public Long fromUser;
     public String body;
-    public LocalDateTime timeQuestionAsked;
 }
