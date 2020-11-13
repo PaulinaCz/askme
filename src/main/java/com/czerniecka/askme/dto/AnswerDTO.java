c5fed3ecd35d526588d78200ed3e761b8346371e
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-11-13 19:04:45 +0000

    added input validation

--- src/main/java/com/czerniecka/askme/dto/AnswerDTO.java
+++ src/main/java/com/czerniecka/askme/dto/AnswerDTO.java
@@ -1,10 +1,18 @@
 package com.czerniecka.askme.dto;
 
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class AnswerDTO {
 
+    @NotNull(message = "Could not answer. Try again")
     public Long userId;
+    @NotNull(message = "Choose question you want to answer")
     public Long questionId;
+    @NotBlank(message = "Please add content to your answer")
+    @Size(max = 5000, message = "Answer should be up to 5000 characters!")
     public String body;
 
 }
