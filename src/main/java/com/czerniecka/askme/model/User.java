e8dceafeb6e07fdc985e4d6951b10adb3481af3d
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-11-13 13:15:23 +0000

    added id generation strategy

--- src/main/java/com/czerniecka/askme/model/User.java
+++ src/main/java/com/czerniecka/askme/model/User.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     private String name;
     private String surname;
