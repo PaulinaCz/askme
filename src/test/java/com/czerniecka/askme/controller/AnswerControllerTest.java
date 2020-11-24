03166d6700ed1f8aa600e4d0bf3774c7771c7530
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-11-24 17:36:17 +0000

    implemented answer controller integration tests

--- src/test/java/com/czerniecka/askme/controller/AnswerControllerTest.java
+++ src/test/java/com/czerniecka/askme/controller/AnswerControllerTest.java
@@ -2,6 +2,7 @@ package com.czerniecka.askme.controller;
 
 import com.czerniecka.askme.TestAuthenticatedUser;
 import com.czerniecka.askme.dto.AnswerDTO;
+import com.czerniecka.askme.dto.LoginUserDTO;
 import com.czerniecka.askme.dto.RatingDTO;
 import com.czerniecka.askme.dto.ShowAnswerDTO;
 import com.czerniecka.askme.model.Rating;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.context.support.WithUserDetails;
@@ -35,6 +37,9 @@ class AnswerControllerTest {
     @Autowired
     AnswerController answerController;
 
+    @Autowired
+    AuthController authController;
+
 
     @Test
     void shouldReturn403WhenUserNotAuthenticated() throws Exception {
@@ -45,15 +50,19 @@ class AnswerControllerTest {
     }
 
     @Test
-    @WithUserDetails
-    void getAnswersByUser() throws Exception {
+    void shouldReturn404WhenNotFound() {
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/answers/answer/1"))
-                .andExpect(status().isNotFound());
+        authUser.authenticatedUser("john", "Password123.");
+
+        ResponseEntity<ShowAnswerDTO> response = answerController.getAnswerById(30L);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+
+
     }
 
     @Test
-    void rate() {
+    void shouldUpdateRating() {
 
         AnswerDTO answerDTO = new AnswerDTO();
         answerDTO.body = "Yup.";
