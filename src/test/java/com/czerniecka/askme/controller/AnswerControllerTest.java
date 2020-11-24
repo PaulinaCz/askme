7b6469c5e47cbe778b258bcc50dd93ebf866fb3b
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-11-24 17:25:51 +0000

    implemented answer controller integration tests

--- src/test/java/com/czerniecka/askme/controller/AnswerControllerTest.java
+++ src/test/java/com/czerniecka/askme/controller/AnswerControllerTest.java
@@ -1,14 +1,23 @@
 package com.czerniecka.askme.controller;
 
+import com.czerniecka.askme.TestAuthenticatedUser;
+import com.czerniecka.askme.dto.AnswerDTO;
+import com.czerniecka.askme.dto.RatingDTO;
+import com.czerniecka.askme.dto.ShowAnswerDTO;
+import com.czerniecka.askme.model.Rating;
 import com.czerniecka.askme.service.AnswerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,6 +29,13 @@ class AnswerControllerTest {
     @Autowired
     AnswerService answerService;
 
+    @Autowired
+    TestAuthenticatedUser authUser;
+
+    @Autowired
+    AnswerController answerController;
+
+
     @Test
     void shouldReturn403WhenUserNotAuthenticated() throws Exception {
 
@@ -29,10 +45,30 @@ class AnswerControllerTest {
     }
 
     @Test
-    void getAnswersByUser() {
+    @WithUserDetails
+    void getAnswersByUser() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/answers/answer/1"))
+                .andExpect(status().isNotFound());
     }
 
     @Test
     void rate() {
+
+        AnswerDTO answerDTO = new AnswerDTO();
+        answerDTO.body = "Yup.";
+        UserDetails user = authUser.authenticatedUser("john", "Password123.");
+        RatingDTO rating = new RatingDTO();
+        rating.rate = Rating.USEFUL.getRate();
+
+        ResponseEntity<Long> longResponse = answerController.addAnswer(answerDTO, 1L, user);
+        Long index = longResponse.getBody();
+        answerController.rate(index, rating);
+        ResponseEntity<ShowAnswerDTO> response = answerController.getAnswerById(index);
+        ShowAnswerDTO aDTO = response.getBody();
+
+        assertEquals(1, aDTO.rating);
+
+
     }
 }
